694acf06cfab4951839c026ba64af1621acf96d3
Thalia-guedes <thalia.guedes@zup.com.br>
2023-04-25 16:27:05 -0300

    testeinteliji

---
+++ src/Bolo.java
@@ -0,0 +1,17 @@
+public class Bolo {
+}       Pré-aqueça o forno a 180°C.
+
+        Em uma tigela grande, misture o açúcar, a farinha de trigo, o cacau em pó, o bicarbonato de sódio e o sal.
+
+        Adicione os ovos, o leite,
+        o óleo vegetal e o extrato de baunilha na tigela com os ingredientes secos e misture tudo com uma batedeira
+        elétrica até ficar homogêneo.
+
+        Adicione a água quente e bata novamente.
+
+        Despeje a mistura em uma forma untada e enfarinhada.
+
+        Asse no forno por cerca de 35-40 minutos,
+        ou até que o bolo esteja assado e o palito saia limpo quando inserido no centro do bolo.
+
+        Deixe o bolo esfriar antes de decorar com glacê, chocolate ou qualquer outro acompanhamento de sua preferência.
---
+++ src/Remedio.java
@@ -0,0 +1,9 @@
+public class Remedio {
+}
+
+Retire o Carro da Garagem
+Dirija ate a farmacia mais proxima
+Ao chegar a farmacia estacione
+Va ate o farmaceutico e peca o remedio desejado
+Apos ser entregue, dirija-se ate o caixa
+Efetue o pagamento, e depois retorne para casa.
